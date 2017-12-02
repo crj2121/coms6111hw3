b7e865ed52facb5690d4d3d8fc378b2b3a73b125
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-12-01 21:56:06 -0500

    done w output

--- driver3.py
+++ driver3.py
@@ -1,11 +1,25 @@
 import sys
 import csv
 
+def apriori(records, things, mSupport, mConfidence):
+	return [(0,0)], [((1, 1), 1, 1)]
+	
 data = sys.argv[1]
-minSupp = float(sys.argv[2])
-minConf = float(sys.argv[3])
+mSupport = float(sys.argv[2])
+mConfidence = float(sys.argv[3])
+records = []
+things = []
 with open(data, newline='') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
+		records.append(row)
 		for thing in row:
-			print(thing)
+			things.append(thing)
+newThings, newRules = apriori(records, things, mSupport, mConfidence)
+output = open('output.txt', 'w')
+output.write("==Frequent itemsets (min_sup=" + str(float(mSupport)*100) + "%)\n")
+for thing in newThings:
+	output.write("[" + str(thing[0]) + "]" + ", " + str(int(float(thing[1])*100)) + "%\n")
+output.write("==High-confidence association rules (min_conf=" + str(float(mConfidence)*100) + "%)\n")
+for rule in newRules:
+	output.write("[" + str(rule[0][0]) + "]" + " => " + "[" + str(rule[0][1]) + "] (Conf: " + str(float(rule[1])*100) + "%, Supp: " + str(int(float(rule[2])*100)) + "%)\n")
