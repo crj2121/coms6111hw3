9f9649f516de1350686d310f9699e79d597a5a1e
Candace Johnson <crj2121@columbia.edu>
2017-12-03 22:14:42 -0500

    apriori in the works

--- driver3.py
+++ driver3.py
@@ -1,7 +1,83 @@
 import sys
 import csv
 
+#collect the frequency of each item
+itemfreq  = {} #(setCount)
+#preserves dictionary order 
+order = {} # largeOne
+everything = {} #largeSet
+import itertools
+
 def apriori(records, things, mSupport, mConfidence):
+	#check which items are above mSupport
+	selected = []
+	current= {}
+	trans = 0
+	uniqueThings = list(set(things))
+	for item in uniqueThings:
+		for record in records:
+			val = True
+			for i in item:
+				if i in record:
+					record.remove(i)
+				else:
+					val = False
+
+			if item in record or val:
+				if item in itemfreq:
+					itemfreq[item] += 1
+				else:
+					itemfreq[item] = 0
+				if item in current:
+					current[item] += 1
+				else:
+					current[item] = 0
+        #finding items that are greater than mSupport
+		print('h')
+		for pair in current.items():
+			total = len(records)
+			if float((pair[1])/total) > mSupport:
+				selected.append((pair[0]))
+		print('a')
+
+	print(selected)
+	totalTrans = 0
+	for record in records:
+		trans += 1
+		recLen = len(record)
+		totalTrans = recLen
+		currItems = []
+	i=0
+	while i < recLen:
+		x=0
+		while x < len(selected):
+			if record[i] in selected[x]:
+				select = record[i]
+				currItems.append(select)
+			x=x+1
+			order[trans] = currItems
+		i = i+1
+
+	for i in range(1,totalTrans +1):
+		possible = []
+		for pair in order.items():
+			combos = []
+			combos = itertools.combinations(pair[1],i)
+			for combo in combos:
+				possible.append(combo)
+		for p in possible:
+			newCurrent = []
+			newCurrent.append(tuple(p))
+    	#where there's all 
+
+
+
+
+
+
+
+		
+
 	return [(0,0)], [((1, 1), 1, 1)]
 	
 data = sys.argv[1]
@@ -15,6 +91,7 @@ with open(data, newline='') as csvfile:
 		records.append(row)
 		for thing in row:
 			things.append(thing)
+
 newThings, newRules = apriori(records, things, mSupport, mConfidence)
 output = open('output.txt', 'w')
 output.write("==Frequent itemsets (min_sup=" + str(float(mSupport)*100) + "%)\n")
