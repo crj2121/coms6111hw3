4b1ecccc7c7c4333c41c730954cc993709d31f51
Gabriel Kramer-Garcia <glk2110@columbia.edu>
2017-12-04 09:36:22 -0500

    WE

--- driver3.py
+++ driver3.py
@@ -134,7 +134,9 @@ with open(data, newline='') as csvfile:
 newThings, newRules = apriori(records, things, mSupport, mConfidence)
 output = open('output.txt', 'w')
 output.write("==Frequent itemsets (min_sup=" + str(float(mSupport)*100) + "%)\n")
-for thing in newThings:
+it = iter(newThings)
+newerThings = zip(it, it)
+for thing in newerThings:
 	output.write("[" + str(thing[0]) + "]" + ", " + str(int(float(thing[1])*100)) + "%\n")
 output.write("==High-confidence association rules (min_conf=" + str(float(mConfidence)*100) + "%)\n")
 for rule in newRules:
